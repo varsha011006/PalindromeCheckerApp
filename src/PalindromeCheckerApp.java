b9339db640eae15c4bd302598646b5cbe3852fa4
Skygazer1111 <pr9348@srmist.edu.in>
2026-02-23 09:55:58 +0530

    UC1 & UC2 completed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,12 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App");
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
